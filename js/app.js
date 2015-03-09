aba2eaad64524455a1fb7be9fc6d5b95971aa848
mandyreal <normandy.real@gmail.com>
2015-03-09 14:28:54 +0800

    used bootstrap to display place listing, added filter box, formatted search box

--- js/app.js
+++ js/app.js
@@ -16,10 +16,18 @@ var viewModel = function() {
     }
   });
 
+// self.listToppicks = ko.computed(function() {
+//    for (var i in self.topPicks()) {
+//      console.log("in listToppicks.." + self.topPicks()[i].venue.name);
+//    }
+//    self.topPicks();
+//  });
+
   function initializeMap() {
     var mapOptions = {
-      zoom: 15
-//      center: new google.maps.LatLng(-34.397, 150.644)
+      zoom: 15,
+      disableDefaultUI: true,
+      // center: new google.maps.LatLng(-34.397, 150.644)
     };
   
     map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
@@ -37,16 +45,14 @@ var viewModel = function() {
 
     foursquareUri = "https://api.foursquare.com/v2/venues/explore?ll=";
     baseLocation = lat + ", " + longt;
-    extraParams = "&limit=15&section=topPicks&day=any&time=any&locale=en&oauth_token=5WJZ5GSQURT4YEG251H42KKKOWUNQXS5EORP2HGGVO4B14AB&v=20141121";
+    extraParams = "&limit=15&section=topPicks&day=any&time=any&locale=en&client_id=1AMO01APWLHPX1VNICMSOD2VWXQSDRUMFDSVTSTJZITY2ZQE&client_secret=XYSEVN4ABIDAWIULKL14TM5GABGQP3EWUOYHOH4XIXSWK2JT&v=YYYYMMDD&v=20141121";
     foursquareQueryUri = foursquareUri + baseLocation + extraParams;
     $.getJSON(foursquareQueryUri, function(data) {
       self.topPicks(data.response.groups[0].items);
       for (var i in self.topPicks()) {
         console.log("FS API return : " + self.topPicks()[i].venue.name + 
-                                         self.topPicks()[i].venue.location.lat);
-         var posi = new google.maps.LatLng(self.topPicks()[i].venue.location.lat, 
-                                               self.topPicks()[i].venue.location.lng);
-         createMarker(posi);
+                                         self.topPicks()[i].venue.location.lat + "counter = " + i);
+        createMarker(self.topPicks()[i].venue);
       }
     });
 
@@ -62,11 +68,17 @@ var viewModel = function() {
     }
   }
 
-  function createMarker(posi) {
+  function createMarker(venue) {
+    var lat = venue.location.lat;
+    var lng = venue.location.lng;
+    var name = venue.name;
+    var position = new google.maps.LatLng(lat, lng);
+
     var marker = new google.maps.Marker({
       map: map,
-      position: posi
-    })
+      position: position,
+      title: name
+    });
   }
 
   // get neighborhood location data using Google Map Place Service

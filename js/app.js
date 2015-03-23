1454993facb2549ee77be336e3d72af90a0e4bb8
mandyreal <normandy.real@gmail.com>
2015-03-23 18:03:15 +0800

    include google map auto resize

--- js/app.js
+++ js/app.js
@@ -89,6 +89,12 @@ var viewModel = function() {
   
     map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
+    google.maps.event.addDomListener(window, "resize", function() {
+     var center = map.getCenter();
+     google.maps.event.trigger(map, "resize");
+     map.setCenter(center); 
+    });
+    
     infowindow = new google.maps.InfoWindow();
   }
 
